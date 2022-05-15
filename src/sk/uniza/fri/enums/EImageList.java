c971f82156060d659f943760cc749dab6f261db6
peto123455 <peto123455@gmail.com>
2022-05-15 18:11:25 +0200

    Resources reorganization

--- src/sk/uniza/fri/enums/EImageList.java
+++ src/sk/uniza/fri/enums/EImageList.java
@@ -5,24 +5,24 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public enum EImageList {
-    MENU("/textures/menu.png"),
-    PLAYER("/textures/player.png"),
-    KNIGHT("/textures/knight.png"),
-    KNIGHT_DEAD("/textures/knight_dead.png"),
-    COINS("/textures/coins.png"),
-    SLASH1("/textures/slash1.png"),
-    SLASH2("/textures/slash2.png"),
-    SLASH3("/textures/slash3.png"),
-    HEART("/textures/heart.png"),
-    HEALTH_POTION("/textures/healthPotion.png"),
-    CHEST("/textures/chest.png"),
-    CHEST_OPEN("/textures/chest_open.png"),
-    WIZARD("/textures/wizard.png"),
-    GOBLIN("/textures/goblin.png"),
-    MAGE_BOSS("/textures/mageBoss.png"),
-    MAGE_BOSS_DEAD("/textures/mageBossDead.png"),
-    PROJECILE_MAGE("/textures/ProjectileMage.png"),
-    PORTAL("/textures/portal.png");
+    MENU("/textures/ui/menu.png"),
+    PLAYER("/textures/entities/creatures/player.png"),
+    KNIGHT("/textures/entities/creatures/knight.png"),
+    KNIGHT_DEAD("/textures/entities/creatures/knight_dead.png"),
+    COINS("/textures/entities/itemsAndChests/coins.png"),
+    SLASH1("/textures/entities/particlesAndProjectiles/slash1.png"),
+    SLASH2("/textures/entities/particlesAndProjectiles/slash2.png"),
+    SLASH3("/textures/entities/particlesAndProjectiles/slash3.png"),
+    HEART("/textures/ui/heart.png"),
+    HEALTH_POTION("/textures/entities/itemsAndChests/healthPotion.png"),
+    CHEST("/textures/entities/itemsAndChests/chest.png"),
+    CHEST_OPEN("/textures/entities/itemsAndChests/chest_open.png"),
+    WIZARD("/textures/entities/creatures/wizard.png"),
+    GOBLIN("/textures/entities/creatures/goblin.png"),
+    MAGE_BOSS("/textures/entities/creatures/mageBoss.png"),
+    MAGE_BOSS_DEAD("/textures/entities/creatures/mageBossDead.png"),
+    PROJECILE_MAGE("/textures/entities/itemsAndChests/ProjectileMage.png"),
+    PORTAL("/textures/entities/itemsAndChests/portal.png");
 
     private BufferedImage image;
 
--- src/sk/uniza/fri/enums/ETileList.java
+++ src/sk/uniza/fri/enums/ETileList.java
@@ -5,14 +5,14 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public enum ETileList {
-    WALL1("/textures/wall_1.png", 1, true),
-    WALL2("/textures/wall_2.png", 2, true),
-    WALL_CRACK("/textures/wall_crack.png", 3, true),
-    FLOOR1("/textures/floor_1.png", 4, false),
-    FLOOR2("/textures/floor_2.png", 5, false),
-    FLOOR3("/textures/floor_3.png", 6, false),
-    FLOOR4("/textures/floor_4.png", 7, false),
-    FLOOR5("/textures/floor_5.png", 8, false);
+    WALL1("/textures/blocks/wall_1.png", 1, true),
+    WALL2("/textures/blocks/wall_2.png", 2, true),
+    WALL_CRACK("/textures/blocks/wall_crack.png", 3, true),
+    FLOOR1("/textures/blocks/floor_1.png", 4, false),
+    FLOOR2("/textures/blocks/floor_2.png", 5, false),
+    FLOOR3("/textures/blocks/floor_3.png", 6, false),
+    FLOOR4("/textures/blocks/floor_4.png", 7, false),
+    FLOOR5("/textures/blocks/floor_5.png", 8, false);
 
     private BufferedImage image;
     private int id;
